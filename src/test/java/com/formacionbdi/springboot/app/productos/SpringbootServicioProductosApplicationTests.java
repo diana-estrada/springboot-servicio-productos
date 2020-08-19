018841e4aa829e3e0970fb3cb0dbe8d1ad86c5da
diana.estrada <diana.estrada.hdez@gmail.com>
2020-08-19 12:50:45 -0500

    testing controller

--- src/test/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplicationTests.java
+++ src/test/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplicationTests.java
@@ -1,16 +1,31 @@
 package com.formacionbdi.springboot.app.productos;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.formacionbdi.springboot.app.productos.controllers.ProductoController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebMvcTest(controllers = ProductoController.class)
 public class SpringbootServicioProductosApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+	
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		this.mockMvc.perform(get("/listar")).andDo(print()).andExpect(status().isOk());
 	}
+	
 
 }
